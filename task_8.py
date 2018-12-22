dc5d6736624a1f59b58ac13217711186689c2302
Saf4ik <Saf4ik85@gmail.com>
2018-12-22 12:42:05 +0300

    Добавлено решение путем динамического программирования

--- task_8.py
+++ task_8.py
@@ -14,3 +14,17 @@ def fib(n):
 n = int(input("Введите целое число: "))
 x = fib(n)
 print(x)
+
+
+""" Вариант решения используя одноменое динамиеское программирование
+    Асимтотика O(n).
+"""
+def fib(n):
+    Fib = [0, 1] + [0] * (n-1)
+    for i in range(2, n+1):
+        Fib[i] = Fib[i-1] + Fib[i-2]
+    return Fib[n]
+
+n = int(input("Введите целое число: "))
+x = fib(n)
+print(x)
